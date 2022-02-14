1c860a64d00a44a4a2538ccd955c1ad8703d0995
Magdalena <sztajner.m123@gmail.com>
2022-02-14 17:06:16 +0100

    dodanie czesci petli, if i elif

--- package.py
+++ package.py
@@ -6,3 +6,18 @@ suma_pustych_kg = 0
 puste_kg = 0
 razem_kg = 0
 
+while waga_elementu > 0 and waga_elementu <= 10:
+    waga_paczki += waga_elementu
+
+    if waga_paczki < 20:
+        print("Dobieram element")
+        # waga_ostatniego_elementu = waga_paczki
+
+    elif waga_paczki == 20:
+        print("Idealnie! Wysyłam")
+        razem_kg += waga_paczki
+        puste_kg = 20 - waga_paczki
+        suma_pustych_kg += puste_kg
+        wyslane_paczki = wyslane_paczki + 1
+        waga_paczki = 0
+        waga_ostatniego_elementu = 0
